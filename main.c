0e0620ff0d610bbfd685ae1a8f45f3bf3cf7f944
lepar <nlzanutim@yahoo.com>
2014-12-05 15:28:28 -0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "hcliente.h"
+#include "hfuncionario.h"
+#include "hproduto.h"
+
+int main(int argc, const char * argv[])
+{
+    menuprincipalprojeto();
+    return 0;
+}
